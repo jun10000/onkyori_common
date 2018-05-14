2e48344842d0f2ec2268f489e1e826ae2d9301d1
jun10000 <jun10000@icloud.com>
2018-05-14 17:56:06 +0900

    Ver. 0.1

---
+++ CreateDBTable.sql
@@ -0,0 +1,11 @@
+/*
+ * Author: jun10000 (https://github.com/jun10000)
+ */
+
+create table signals
+(
+  id    int auto_increment
+    primary key,
+  clock datetime(3) null,
+  name  varchar(20) not null
+);
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # onkyori_common
 This is the system what enables cooperation with the Onkyo RI system.
+
+2017/5/14<br>
+I'm writing this README file.
